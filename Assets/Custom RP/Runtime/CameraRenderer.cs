dac39c6bf98227347f01e69f577706525b12a0b5
ezgm1220 <2431510525@qq.com>
2023-10-06 17:01:46 +0800

    1

---
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public partial class CameraRenderer
+{
+
+    const string bufferName = "Render Camera";
+
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
+    CommandBuffer buffer = new CommandBuffer
+    {
+        name = bufferName
+    };
+
+    ScriptableRenderContext context;
+
+    Camera camera;
+
+    CullingResults cullingResults;// 剔除结果
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+
+        PrepareBuffer();// 对每个摄像机使用不同的Sample Name
+        PrepareForSceneWindow();// 绘制Scene窗口下的UI
+        if (!Cull())
+        {
+            return;
+        }
+
+        Setup();
+        DrawVisibleGeometry();
+        DrawUnsupportedShaders();
+        DrawGizmos();
+        Submit();
+
+    }
+
+    void DrawVisibleGeometry()
+    {
+        //决定物体绘制顺序是正交排序还是基于深度排序的配置
+        var sortingSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+
+        //决定摄像机支持的Shader Pass和绘制顺序等的配置
+        var drawingSettings = new DrawingSettings(unlitShaderTagId, sortingSettings);
+
+        //决定过滤哪些Visible Objects的配置，包括支持的RenderQueue等
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+        //渲染CullingResults内不透明的VisibleObjects
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+        
+        //添加“绘制天空盒”指令，DrawSkybox为ScriptableRenderContext下已有函数，这里就体现了为什么说Unity已经帮我们封装好了很多我们要用到的函数，SPR的画笔~
+        context.DrawSkybox(camera);
+        
+        //渲染透明物体
+        //设置绘制顺序为从后往前
+        sortingSettings.criteria = SortingCriteria.CommonTransparent;
+       
+        //注意值类型
+        drawingSettings.sortingSettings = sortingSettings;
+        
+        //过滤出RenderQueue属于Transparent的物体
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+        
+        //绘制透明物体
+        context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
+    }
+
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+        return false;
+    }
+
+    void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        CameraClearFlags flags = camera.clearFlags;
+        buffer.ClearRenderTarget(
+            flags <= CameraClearFlags.Depth,
+            flags <= CameraClearFlags.Color,
+            flags == CameraClearFlags.Color ?
+                camera.backgroundColor.linear : Color.clear
+        );
+        buffer.BeginSample(SampleName);
+        ExecuteBuffer();
+    }
+
+    void Submit()
+    {
+        buffer.EndSample(SampleName);
+        ExecuteBuffer();
+        context.Submit();
+    }
+
+    void ExecuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+
+}
