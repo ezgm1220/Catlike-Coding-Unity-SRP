b53561a41d7cae3bef31a0e56a5852689a40a771
ezgm1220 <2431510525@qq.com>
2023-10-07 15:48:13 +0800

    2.3 Transparency

--- Assets/Custom RP/Examples/PerObjectMaterialProperties.cs
+++ Assets/Custom RP/Examples/PerObjectMaterialProperties.cs
@@ -5,15 +5,15 @@ public class PerObjectMaterialProperties : MonoBehaviour
 {
 
     static int baseColorId = Shader.PropertyToID("_BaseColor");
-    //static int cutoffId = Shader.PropertyToID("_Cutoff");
+    static int cutoffId = Shader.PropertyToID("_Cutoff");
 
     static MaterialPropertyBlock block;
 
     [SerializeField]
     Color baseColor = Color.white;
 
-    //[SerializeField, Range(0f, 1f)]
-    //float alphaCutoff = 0.5f;
+    [SerializeField, Range(0f, 1f)]
+    float alphaCutoff = 0.5f;
 
     void Awake()
     {
@@ -27,7 +27,7 @@ public class PerObjectMaterialProperties : MonoBehaviour
             block = new MaterialPropertyBlock();
         }
         block.SetColor(baseColorId, baseColor);
-        //block.SetFloat(cutoffId, alphaCutoff);
+        block.SetFloat(cutoffId, alphaCutoff);
         GetComponent<Renderer>().SetPropertyBlock(block);
     }
 }
