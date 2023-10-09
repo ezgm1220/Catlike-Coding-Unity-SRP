b929df4550c02cac2bdef8547bbea8650bf9eec0
ezgm1220 <2431510525@qq.com>
2023-10-09 18:25:25 +0800

    test

--- Assets/Custom RP/ShaderLibrary/Common.hlsl
+++ Assets/Custom RP/ShaderLibrary/Common.hlsl
@@ -10,7 +10,7 @@
 #define UNITY_MATRIX_V unity_MatrixV
 #define UNITY_MATRIX_I_V unity_MatrixInvV
 #define UNITY_MATRIX_VP unity_MatrixVP
-#define UNITY_PREV_MATRIX_M unity_prev_MatrixM
+#define UNITY_PREV_MATRIX_M unity_prev_MatrixM 
 #define UNITY_PREV_MATRIX_I_M unity_prev_MatrixIM
 #define UNITY_MATRIX_P glstate_matrix_projection
 
